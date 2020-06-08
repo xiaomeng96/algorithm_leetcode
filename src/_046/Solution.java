5a933a25f349cd4049b4da283a889e04cd24df20
xiaomeng96 <xiaomeng675@Gmail.com>
2020-06-08 13:09:13 +0800

    46. 全排列

---
+++ src/_046/Solution.java
@@ -0,0 +1,55 @@
+package _046;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 46. 全排列
+ * @author xiaomeng
+ *
+ */
+public class Solution {
+	/**
+	 * 回溯算法（深度优先遍历+状态重置）
+	 * @param nums
+	 * @return
+	 */
+    public List<List<Integer>> permute(int[] nums) {
+    	
+    	List<List<Integer>> res=new ArrayList<>();
+    	int len = nums.length;
+    	if(len == 0)
+    		return res;
+    	int[] visited =new int[len];
+    	backtrack(nums, res, new ArrayList<>(), visited);
+    	return res;
+    	
+    }
+    
+    public void backtrack(int[] nums, List<List<Integer>> res, ArrayList<Integer> path, int[] visited) {
+    	//递归出口
+    	if(path.size() == nums.length) {
+    		res.add(new ArrayList<>(path));
+    		return;
+    	}
+    	for (int i = 0; i < nums.length; i++) {
+    		if(visited[i] == 1)
+    			continue;
+    		path.add(nums[i]);
+    		visited[i] = 1;
+    		backtrack(nums, res, path, visited);//固定一个数，继续去找下一个
+    		//重置
+    		visited[i] = 0;
+    		path.remove(path.size()-1);			
+		}
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution so=new Solution();
+		int[] nums= {1,2,3};
+		List<List<Integer>> res=so.permute(nums);
+		System.out.println(res);
+
+	}
+
+}
